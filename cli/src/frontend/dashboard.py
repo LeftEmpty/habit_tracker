a0c3db9e21bd00f07fbf4defbcc62c4c80162a81
LeftEmpty <pocralol@gmail.com>
2025-04-08 04:03:05 +0200

    changed file structure & added panel prompts, wip

---
+++ cli/src/frontend/dashboard.py
@@ -0,0 +1,12 @@
+
+
+class Dashboard:
+    def __init__(self)->None:
+        print(" ello mate ")
+        
+    
+    def OpenCLI():    
+        
+    # @TODO query data of given day for logged in user
+    
+    # @TODO allow user to tick them by displaying relevant options
