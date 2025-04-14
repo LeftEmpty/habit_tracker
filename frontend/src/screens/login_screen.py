26771aacecdb3a107ada39dc358ea909f07aadaf
LeftEmpty <pocralol@gmail.com>
2025-04-14 11:33:07 +0200

    rename "Panel" to "PanelState", justified labels to left

--- frontend/src/screens/login_screen.py
+++ frontend/src/screens/login_screen.py
@@ -6,14 +6,14 @@ from util.src.helper import validate_input
 from frontend.src.screens.screen import ScreenBase
 
 
-class Panel(Enum):
+class PanelState(Enum):
     LOGIN = "Login"
     REGISTER = "Register"
 
 
 class LoginScreen(ScreenBase):
     def __init__(self, root_reference:tk.Tk) -> None:
-        self.cur_panel = Panel.LOGIN
+        self.cur_panel = PanelState.LOGIN
         super().__init__(root_reference) # type: ignore
 
 
@@ -25,32 +25,32 @@ class LoginScreen(ScreenBase):
         self.config(bg="#000")
 
         # Widgets
-        self.title_label = tk.Label(self, text="LOGIN", bg='#000', fg="#fff", font=("Roboto", 34))
-        self.email_label = tk.Label(self, text="Email", bg='#000', fg="#fff", font=("Roboto", 13))
+        self.title_label = tk.Label(self, text="LOGIN", bg='#000', fg="#fff", font=("Roboto", 34), anchor="w")
+        self.email_label = tk.Label(self, text="Email", bg='#000', fg="#fff", font=("Roboto", 13), anchor="w")
         self.email_entry = tk.Entry(self)
-        self.username_label = tk.Label(self, text="Username", bg='#000', fg="#fff", font=("Roboto", 13))
+        self.username_label = tk.Label(self, text="Username", bg='#000', fg="#fff", font=("Roboto", 13), anchor="w")
         self.username_entry = tk.Entry(self)
-        self.password_label = tk.Label(self, text="Password", bg='#000', fg="#fff", font=("Roboto", 13))
+        self.password_label = tk.Label(self, text="Password", bg='#000', fg="#fff", font=("Roboto", 13), anchor="w")
         self.password_entry = tk.Entry(self, show="*")
-        self.password2_label = tk.Label(self, text="Confirm Password", bg='#000', fg="#fff", font=("Roboto", 13))
+        self.password2_label = tk.Label(self, text="Confirm Password", bg='#000', fg="#fff", font=("Roboto", 13), anchor="w")
         self.password2_entry = tk.Entry(self, show="*")
         self.login_button = tk.Button(self, text="Login", fg='#fff', bg='#000', borderwidth=2, relief="flat", command=self.try_login)
         self.register_button = tk.Button(self, text="Register", fg='#fff', bg='#000', borderwidth=2, relief="flat", command=self.try_register)
 
         # Set Layout
-        self.title_label.grid(row=0, column=0, columnspan=2, pady=8, padx=8)
-        self.email_label.grid(row=1, column=0)
+        self.title_label.grid(row=0, column=0, columnspan=2, pady=8, padx=8, sticky="w")
+        self.email_label.grid(row=1, column=0, padx=8, sticky="w")
         self.email_entry.grid(row=1, column=1, columnspan=2)
-        self.username_label.grid(row=2, column=0)
+        self.username_label.grid(row=2, column=0, padx=8, sticky="w")
         self.username_entry.grid(row=2, column=1, columnspan=2)
-        self.password_label.grid(row=3, column=0)
+        self.password_label.grid(row=3, column=0, padx=8, sticky="w")
         self.password_entry.grid(row=3, column=1, columnspan=2)
-        self.password2_label.grid(row=4, column=0)
+        self.password2_label.grid(row=4, column=0, padx=8, sticky="w")
         self.password2_entry.grid(row=4, column=1, columnspan=2)
         self.login_button.grid(row=5, column=0, pady=12)
         self.register_button.grid(row=5, column=2, pady=12)
 
-        # Updatee Layout to reflect current panel (init LOGIN)
+        # Updatee Layout to reflect current panel state (init LOGIN)
         self.update_screen(self.cur_panel)
 
         # debug / info
@@ -60,9 +60,9 @@ class LoginScreen(ScreenBase):
     def try_login(self) -> None:
         """"""
         # check we're on the right panel, update if necessary
-        if self.cur_panel == Panel.REGISTER:
+        if self.cur_panel == PanelState.REGISTER:
             # update screen
-            self.update_screen(Panel.LOGIN)
+            self.update_screen(PanelState.LOGIN)
             return
 
         # get input & convert to string (type check)
@@ -88,14 +88,15 @@ class LoginScreen(ScreenBase):
 
         # query db via action handler
         # @TODO call action handler login function
+        #if (action_handler.login()):
 
 
     def try_register(self) -> None:
         """"""
         # check we're on the right panel, update if necessary
-        if self.cur_panel == Panel.LOGIN:
+        if self.cur_panel == PanelState.LOGIN:
             # update screen
-            self.update_screen(Panel.REGISTER)
+            self.update_screen(PanelState.REGISTER)
             return
 
         # get input & convert to string (type check)
@@ -130,14 +131,14 @@ class LoginScreen(ScreenBase):
         self.title_label.config(text=to_panel.value)
 
         # adjust layout
-        if to_panel == Panel.LOGIN:
+        if to_panel == PanelState.LOGIN:
             # Layout
             self.email_label.grid_remove()
             self.email_entry.grid_remove()
             self.password2_label.grid_remove()
             self.password2_entry.grid_remove()
 
-        elif to_panel == Panel.REGISTER:
+        elif to_panel == PanelState.REGISTER:
             # Layout
             self.email_label.grid()
             self.email_entry.grid()
