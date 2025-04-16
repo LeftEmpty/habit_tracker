2c3d2c4a89291ea72b5bbbce1f6651a3072c6827
LeftEmpty <pocralol@gmail.com>
2025-04-16 20:14:56 +0200

    added init db to run.py, also added cleanup atexit but this is unused so far

--- run.py
+++ run.py
@@ -1,19 +1,24 @@
 import tkinter as tk
 from frontend.src.gui import GUI
+import db.src.controller as dbc
 from util.src.action_handler import ActionHandler
-
+import atexit
 
 def main() -> None:
 
     # Start the SQLite server in a separate process with logging
-    
+
+    dbc.db_init_db()
+
+    #atexit.register(cleanup)
 
     action_handler = ActionHandler()
     gui = GUI(action_handler)
     gui.__start__() # starts mainloop
 
-
-
 if __name__ == "__main__":
     main()
+
+def cleanup() -> None:
+    pass
 
