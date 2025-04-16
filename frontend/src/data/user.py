f6463e795c8f3ca411345d61a1e08c8f7f385ad5
LeftEmpty <pocralol@gmail.com>
2025-04-16 15:18:31 +0200

    moved & updated frontend classes for habits & user

---
+++ frontend/src/data/user.py
@@ -0,0 +1,26 @@
+from frontend.src.data.habits import HabitSubscription, HabitData
+
+class User:
+    def __init__(self, user_id:int, display_name:str, username:str, password:str, email:str, habits:list[HabitSubscription] = []):
+        """_summary_
+
+        Args:
+            user_id (int): _description_
+            display_name (str): _description_
+            username (str): _description_
+            password (str): _description_
+            email (str): _description_
+            habits (list[HabitSubscription], optional): _description_. Defaults to [].
+        """
+        # @TODO update / sync user data with database
+            # @TODO this should also sync streaks, completions, etc.
+        self.user_id:int = user_id
+        self.display_name:str = display_name
+        self.email:str = email
+        self.username = username
+        self.password = password
+        self.habit_subscriptions = habits
+
+    def get_all_subscribed_habits(self) -> list[HabitSubscription]:
+        # @TODO
+        return []
